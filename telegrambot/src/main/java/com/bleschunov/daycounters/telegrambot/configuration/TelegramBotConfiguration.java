8a1d1d640823e236ce41779ff5fe12c94b8822b8
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-01-09 22:04:29 +0300

    make rest uri as external variable

--- telegrambot/src/main/java/com/bleschunov/daycounters/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegrambot/src/main/java/com/bleschunov/daycounters/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,5 +1,6 @@
 package com.bleschunov.daycounters.telegrambot.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,11 +13,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class TelegramBotConfiguration {
 
+    @Value("${rest.uri}")
+    private String restUri;
+
     @Bean
     public WebClient webClient() {
         return WebClient
                 .builder()
-                .baseUrl("http://localhost:8080")
+                .baseUrl(restUri)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
